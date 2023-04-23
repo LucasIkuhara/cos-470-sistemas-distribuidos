5ccb6433e828c74e906caefd638622c2777b54b5
Lucas Ikuhara <lri2911@gmail.com>
2023-04-22 23:55:49 -0300

    feat: read signal and pid

--- signaler/src/main.rs
+++ signaler/src/main.rs
@@ -3,10 +3,17 @@ use std::env;
 
 fn main() {
 
-    for argument in env::args_os() {
-        
-        println!("{}", 
-            argument.into_string().expect("Error reading arguments.")
-        );
-    }
+    // Get all args from argv
+    let mut args =  env::args_os();
+    
+    // Discard first argument, which is the executable path
+    args.next().unwrap();
+
+    // Parse PID and SIGNAL
+    let pid = args.next().expect("Missing PID argument. (run as ./signaler <pid> <signal>)");
+    let signal = args.next().expect("Missing SIGNAL argument. (run as ./signaler <pid> <signal>)");
+   
+    println!("PID: {}", pid.into_string().unwrap());
+    println!("SIGNAL: {}", signal.into_string().unwrap());
+
 }
