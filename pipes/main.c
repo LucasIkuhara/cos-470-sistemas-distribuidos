90be9d7de8375fa80829575cf82e6c57e61e29a0
Lucas Ikuhara <lri2911@gmail.com>
2023-04-24 19:49:55 -0300

    feat: add pipe creation

---
+++ pipes/main.c
@@ -0,0 +1,32 @@
+#include <signal.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <time.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <stdio.h>  
+#include <errno.h> 
+
+
+// Creates a new pipe and save descriptors to the input array
+void criarPipe(int descriptor[]) {
+
+    // Creates a new pipe
+    if(pipe(descriptor) < 0) {
+
+        // Error handling
+        printf("Failed to create a pipe.\n");
+        exit(1);
+    }
+}
+
+
+int main(void) {
+
+    // Creates a new pipe using this int[] as a descriptor
+    int pipeDesc[2];
+    createPipe(pipeDesc);
+    
+
+    return 0;
+}
