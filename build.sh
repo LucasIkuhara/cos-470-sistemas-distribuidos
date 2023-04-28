62453250988f14b26791dc7a29a78bde5a4ed573
Lucas Ikuhara <lri2911@gmail.com>
2023-04-27 22:51:47 -0300

    feat: build script updates

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
-g++ ./pipes/pipes.cpp -o pipes.out
-g++ ./signals/signals.cpp -o signals.out
+g++ ./src/pipes.cpp -o pipes.out
+g++ ./src/signals.cpp -o signals.out
+g++ ./src/sockets.cpp -o sockets.out
