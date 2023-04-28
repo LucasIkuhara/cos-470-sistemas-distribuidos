8f1c569ea290ffd7ec4b81ef1ef5a02f9dcb4ecf
hgknust <100875420+hgknust@users.noreply.github.com>
2023-04-27 21:44:08 -0300

    Adicionar Singal handler

--- sockets.cpp
+++ sockets.cpp
@@ -7,7 +7,8 @@
 #include <arpa/inet.h>
 #include <cmath>
 
-#define PORT 10800
+const int PORT = 10800;
+volatile bool program_terminated = false;
 
 struct Producer {
     int socket_fd{};
@@ -15,6 +16,7 @@ struct Producer {
     int port = PORT;
 
     Producer() {
+        signal(SIGINT, [](int n){ program_terminated = true; });
         create_socket();
         connect_to_consumer();
         srand(time(nullptr));
@@ -24,7 +26,7 @@ struct Producer {
         socket_fd = socket(AF_INET, SOCK_STREAM, 0);
         if (socket_fd < 0) {
             std::cerr << "Error creating socket" << std::endl;
-            exit(1);
+            exit(0);
         }
     }
 
@@ -35,29 +37,47 @@ struct Producer {
 
         if (inet_pton(AF_INET, "127.0.0.1", &consumer_address.sin_addr) <= 0) {
             std::cerr << "Invalid address/ Address not supported" << std::endl;
-            exit(1);
+            exit(0);
         }
 
         if (connect(socket_fd, (struct sockaddr*)&consumer_address, sizeof(consumer_address)) < 0) {
             std::cerr << "Connection Failed" << std::endl;
-            exit(1);
+            exit(0);
         }
     }
 
+    void send_number(int number) {
+        auto message = std::to_string(number);
+        send(socket_fd, message.c_str(), message.length(), 0);
+    }
+
+    std::string receive_response() {
+        char buffer[16] = {0};
+        recv(socket_fd, buffer, sizeof(buffer), 0);
+        return std::string(buffer);
+    }
+
     void produce() {
-        while (true) {
-            int random_num = rand() % 1000 + 1;
-            std::string message = std::to_string(random_num);
+        int last_number = 0;
 
-            send(socket_fd, message.c_str(), message.length(), 0);
+        while (!program_terminated) {
+            auto random_num = last_number + (rand() % 100 + 1);
+            last_number = random_num;
 
-            char buffer[16] = {0};
-            recv(socket_fd, buffer, sizeof(buffer), 0);
+            send_number(random_num);
 
-            std::cout << "Received: " << buffer << std::endl;
+            auto response = receive_response();
+
+            std::cout << "Received: " << response << std::endl;
 
             sleep(1);
         }
+
+        std::cout << "Producer: Received SIGINT. Sending 0 and exiting" << std::endl;
+        send_number(0);
+
+        close(socket_fd);
+        exit(1);
     }
 };
 
@@ -90,14 +110,14 @@ struct Consumer {
 
         if (bind(socket_fd, (struct sockaddr *)&address, addr_len) < 0) {
             std::cerr << "Bind failed" << std::endl;
-            exit(1);
+            exit(0);
         }
     }
 
     void start_listen() {
         if (listen(socket_fd, 3) < 0) {
             std::cerr << "Listen failed" << std::endl;
-            exit(1);
+            exit(0);
         }
     }
 
@@ -121,13 +141,20 @@ struct Consumer {
 
         if ((producer_socket = accept(socket_fd, (struct sockaddr *)&address, (socklen_t*)&addr_len)) < 0) {
             std::cerr << "Accept failed" << std::endl;
-            exit(1);
+            exit(0);
         }
 
         while (true) {
             char buffer[16] = {0};
             recv(producer_socket, buffer, sizeof(buffer), 0);
             int number = std::stoi(buffer);
+
+            if(number == 0) {
+                std::cout << "Consumer: Received 0. Exiting" << std::endl;
+                close(socket_fd);
+                close(producer_socket);
+                exit(1);
+            }
 
             std::cout << "Consumed: " << number << std::endl;
 
@@ -138,6 +165,8 @@ struct Consumer {
 };
 
 int main() {
+    signal(SIGINT, SIG_IGN);
+
     int producer_pid = fork();
     if (producer_pid == 0) {
         Producer producer;
@@ -151,8 +180,7 @@ int main() {
     }
 
     int status;
-    waitpid(producer_pid, &status, 0);
-    waitpid(consumer_pid, &status, 0);
+    wait(&status);
 
     return 0;
 }
